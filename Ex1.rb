37b5e2e40c7df13fc0ac29db971f8f6a54e1fa1d
Morten Kristiansen <morten.kristainsen@gmail.com>
2016-01-14 19:39:05 +0000

    Add additional input/output examples to Ex1
    Practicing gets, puts and integer conversions

--- Ex1.rb
+++ Ex1.rb
@@ -1,3 +1,8 @@
 print("Please enter your name")
 name = gets
 printf("Hello, " + name)
+
+val1 = Integer(gets("Enter a value between 1 and 7"))
+val2 = Integer(gets("Enter a value between 7 and 10"))
+
+puts("val1 plus val2 is: "val1+val2)
