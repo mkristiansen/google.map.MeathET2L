823b4fae2423c62693a5bb6c7392206218aae111
Morten Kristiansen <morten.kristainsen@gmail.com>
2016-02-12 19:29:47 +0000

    Clean up unclean state from git template.

--- file_mimetype.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/ruby
-
-mime_type = `file /Users/ieu94897/SleepServletEar.tar.bz2 --mime-type`.gsub("\n", '')
-printf mime_type
-printf "\n"
-print mime_type.split(':')[1].split('/')[1].gsub('x-', '').gsub(/jpeg/, 'jpg').gsub(/text/, 'txt').gsub(/x-/, '')
